18157ad5b6180444ba0f5fe279e6a2ee333d8d48
Matheus Santos Araújo <wenked.matheus@yahoo.com.br>
2023-04-16 16:10:26 -0300

    add prisma

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,9 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
+import dotev from 'dotenv';
+import { startSock } from './services/wbotService';
+dotev.config();
 
 const app = express();
 app.use(cors());
@@ -15,6 +18,21 @@ app.post('/api/post', (req, res) => {
 	res.send(req.body);
 });
 
+process.on('uncaughtException', (err) => {
+	console.log('uncaughtException');
+	console.error(err);
+});
+
 app.listen(5000, () => {
 	console.log('Listening to 5000');
 });
+
+const startWhatsappSessions = async () => {
+	try {
+		await startSock();
+	} catch (error) {
+		console.log(error);
+	}
+};
+
+startWhatsappSessions();
---
+++ src/utils/sendMessageWtyping.ts
@@ -0,0 +1,15 @@
+import { WASocket, delay, AnyMessageContent } from '@adiwajshing/baileys';
+
+const sendMessageWTyping = async (msg: AnyMessageContent, jid: string, sock: WASocket) => {
+	await sock.presenceSubscribe(jid);
+	await delay(500);
+
+	await sock.sendPresenceUpdate('composing', jid);
+	await delay(2000);
+
+	await sock.sendPresenceUpdate('paused', jid);
+
+	await sock.sendMessage(jid, msg);
+};
+
+export default sendMessageWTyping;
